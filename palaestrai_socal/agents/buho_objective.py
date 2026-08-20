69f7d0b4c5752f2c4a0e739f5dac86a2aa4c2476
Josef Leinweber <josef.leinweber@proton.me>
2026-08-20 11:52:42 +0200

    feat: inital buho draft with placeholder for populate_healthy_houses

--- palaestrai_socal/agents/buho_objective.py
+++ palaestrai_socal/agents/buho_objective.py
@@ -2,4 +2,24 @@ from palaestrai.agent import Objective
 
 
 class BurnedHousesObjective(Objective):
-    def
+    def __init__(self):
+
+        self._healthy_houses: int = populate_healthy_houses()
+        self._scaling: float = 1.0
+
+    def populate_healthy_houses(self) -> int:
+        pass
+
+    def internal_reward(self, memory, **kwargs) -> float:
+        tail = memory.tail(1)
+
+        burned_houses = _get_burned_houses(getattr(tail, "sensor_readings", None))
+
+        if burned_houses == None:
+            return 1.0
+
+        return cacculate_reward(burned_houses)
+
+    def calculate_reward(self, int: burned_houses) -> float:
+        baseline_reward = 1.0
+        return baseline_reward - (burned_houses / self._healthy_houses)
